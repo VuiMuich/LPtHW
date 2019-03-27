4502a5a3eb6377db2830741a3bc294c4163a35f4
Johannes Mayrhofer <johannes.mayrhofer@t-online.de>
2019-03-27 08:25:07 +0100

    Update ex1.py, added ex4.py

--- Exercises/ex1.py
+++ Exercises/ex1.py
@@ -1,2 +1,6 @@
 print("Hello World!")
 print("This is my Exercise1 in LPthW")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
