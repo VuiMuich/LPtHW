27d9dca4137eac5bceb89316b157b3664fae14ce
VuiMuich <jm.spam@gmx,net>
2020-01-14 17:12:56 +0100

    initial für radix-sort projekt und others für diverse andere Übungen z.B. von Socratica

---
+++ Radixsort/radix_lsd_attempt.py
@@ -0,0 +1,5 @@
+def radix_lsd(sortme):
+    """ sort the list "sortme" with the Radix Sort starting with Least Significant Digit, returns sorted list """
+    for i in range(1, sortme):
+
+radix_lsd([170, 45, 75, 90, 802, 24, 2, 66])
